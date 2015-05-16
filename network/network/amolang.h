7771db9a3b6f4c886956c620e5b64e53725fe9af
hjm <hh1733@naver.com>
2015-05-16 14:25:08 +0900

    2015.05.16 study making chat prog

---
+++ network/network/amolang.h
@@ -0,0 +1,15 @@
+// 공통선언부 및 매크로 지정
+//
+#pragma once
+
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+// 공통 매크로 정의
+#define BUFFERSIZE			1024
+
+// 공통 함수 정의
+// 오류메시지 출력
+void DisplayMessage();
